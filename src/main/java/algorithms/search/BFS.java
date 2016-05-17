ba16c222c2b917c2bda0c4699ed8b3d94ba6521e
Tuval Lifshitz <tuvpak@gmail.com>
2016-05-17 19:20:53 +0300

    working on bfs

--- src/main/java/algorithms/search/BFS.java
+++ src/main/java/algorithms/search/BFS.java
@@ -4,23 +4,25 @@ import java.util.LinkedList;
 import java.util.List;
 import algorithms.maze.Maze3d;
 import algorithms.maze.Position;
+import algorithms.search.PositionHelper;
 
 public class BFS implements Search{
 
-	List<Position> toReturn;
+	List<PositionHelper> toReturn;
 	Maze3d myMaze;
 	int [][][] boardHelper;
 	public List<Position>  FindPath(Maze3d maze) {
 		myMaze = maze;
 		Position Start = myMaze.getStartPosition();
 		Position Goal = myMaze.getGoalPosition();
-		toReturn = new LinkedList<Position>();
+		toReturn = new LinkedList<PositionHelper>();
 		boardHelper = createMazeHelper();
 		return FindHelper(Start,Goal);
 	}
 	
 	private  List<Position> FindHelper(Position start, Position Goal){
 		if(start == Goal){
+			
 			toReturn.add(Goal);
 		}
 		else if(start == null){
@@ -29,12 +31,16 @@ public class BFS implements Search{
 		else{
 			
 			for(Position p: myMaze.getNeighborPositions(start)){
+				PositionHelper ph = p;
+				if(p == myMaze.getGoalPosition()){
+					
+				}
 				FindHelper(p, Goal);
 			}
 		}
 	}
 	
-	private int [][][] createMazeHelper(){
+	private PositionHelper [][][] createMazeHelper(){
 		int myHeight = myMaze.getHeight();
 		int myWidth = myMaze.getWidth();
 		int myDepth = myMaze.getDepth();
@@ -47,16 +53,16 @@ public class BFS implements Search{
 		return initializeBoard(myBoard);
 	}
 	
-	private int[][][] initializeBoard(int [][][] Board){
-		int[][][] myBoard = Board;
+	private PositionHelper[][][] initializeBoard(int [][][] Board){
+		PositionHelper[][][] myBoard = new PositionHelper[myMaze.getHeight()][ myMaze.getWidth()][ myMaze.getDepth()];
 		for(int i = 0; i < myMaze.getHeight(); i++){
 			for(int j = 0; j < myMaze.getWidth(); j++){
 				for(int k = 0; k < myMaze.getDepth(); k++){
-					if (myBoard[i][j][k] == 0){
-						myBoard[i][j][k] = Integer.MAX_VALUE;
+					if (Board[i][j][k] == 0){
+						myBoard[i][j][k].setValue(Integer.MAX_VALUE);
 					}
 					else{
-						myBoard[i][j][k] = -1;//stands for wall
+						myBoard[i][j][k].setValue(-1);//stands for wall
 					}
 				}
 			}
